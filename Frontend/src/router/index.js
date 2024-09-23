c51fb2579c7cacc50abc40934b31c4461c215627
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-09-23 14:46:45 +0900

    feat: 커뮤니티 글 작성 페이지 추가

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,22 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/pages/Home.vue';
-import Shop from '@/pages/Shop.vue';
-import Community from '@/pages/community/Community.vue';
-import InfoPlaza from '@/pages/InfoPlaza/InfoPlaza.vue';
-import PopularIndustry from '@/pages/InfoPlaza/PopularIndustry.vue';
-import StoreInfo from '@/pages/InfoPlaza/StoreInfo.vue';
-import Education from '@/pages/InfoPlaza/Education.vue';
-import LoanInfo from '@/pages/InfoPlaza/LoanInfo.vue';
-import news from '@/pages/InfoPlaza/news.vue';
-import Report from '@/pages/report/Report.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/pages/Home.vue";
+import Shop from "@/pages/Shop.vue";
+import Community from "@/pages/community/Community.vue";
+import InfoPlaza from "@/pages/InfoPlaza/InfoPlaza.vue";
+import BoardCreate from '@/pages/community/BoardCreate.vue';
+import PopularIndustry from "@/pages/InfoPlaza/PopularIndustry.vue";
+import StoreInfo from "@/pages/InfoPlaza/StoreInfo.vue";
+import Education from "@/pages/InfoPlaza/Education.vue";
+import LoanInfo from "@/pages/InfoPlaza/LoanInfo.vue";
+import news from "@/pages/InfoPlaza/news.vue";
+import Report from "@/pages/report/Report.vue";
+import MyStore from "@/pages/asset/MyStore.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'Home', component: Home },
-    { path: '/shop', name: 'Shop', component: Shop },
-    { path: '/community', name: 'Community', component: Community },
-    { path: '/infoPlaza', name: 'InfoPlaza', component: InfoPlaza },
+    { path: "/", name: "Home", component: Home },
+    { path: "/shop", name: "Shop", component: Shop },
+    { path: "/community", name: "Community", component: Community },
+    { path: "/community/create", name: "BoardCreate", component: BoardCreate },
+    { path: "/infoPlaza", name: "InfoPlaza", component: InfoPlaza },
     {
       path: '/infoPlaza/industry',
       name: 'Industry',
